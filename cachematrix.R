5f4bc7ba7e80b4f08397a5c8df13f8cfc8e725ab
nmajstorovic <natasa.majstorovic@codecentric.de>
2015-12-25 11:20:52 +0100

    functions for computing and cashing the inverse of matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Computing and cashing the inverse of matrix where x is a square 
+## invertible matrix. 
 
-## Write a short comment describing this function
+
+## This function creates a special "matrix" object that can cache its inverse.
+## argument x is the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverseMatrix) inv <<- inverseMatrix
+    getInverse <- function() inv
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix"  returned by
+## makeCacheMatrix above or retrieves the cashed values if inverse has already
+## been calculated. argument x is the special "matrix"
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inverseMatrix <- x$getInverse()
+    if(!is.null(inverseMatrix)) {
+        message("getting cached data")
+        return(inverseMatrix)
+    }
+    data <- x$get()
+    inverseMatrix <- solve(data, ...)
+    x$setInverse(inverseMatrix)
+    inverseMatrix
 }
